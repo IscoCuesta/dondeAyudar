9d7efb5743952375f97e3243935601e96d59dca4
Isco <Iscocuesta@hotmail.com>
2019-07-31 18:30:32 -0500

    initial commit

---
+++ client/build/precache-manifest.9e93dff0bf3ed5e1195fb0ba183b1c53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0e98ef5853da59ad39f",
+    "url": "/static/js/main.77f92e78.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "c0e98ef5853da59ad39f",
+    "url": "/static/css/main.0a313781.chunk.css"
+  },
+  {
+    "revision": "12dfda8b1246ae8cb92f24fd145a8dfe",
+    "url": "/index.html"
+  }
+];
