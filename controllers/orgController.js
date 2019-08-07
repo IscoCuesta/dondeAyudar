5e894487620c52346ed9557d1fef3eb60b8668eb
Magpie <edchavez821@gmail.com>
2019-08-07 18:24:06 -0500

    mongo models and backend routes

---
+++ controllers/orgController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Organization
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Organization
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Organization
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Organization
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Organization
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
---
+++ routes/api/organizations.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const orgController = require("../../controllers/orgController");
+
+// Matches with "/api/orgs"
+router
+  .route("/")
+  .get(orgController.findAll)
+  .post(orgController.create);
+
+// Matches with "/api/orgs/:id"
+router
+  .route("/:id")
+  .get(orgController.findById)
+  .put(orgController.update)
+  .delete(orgController.remove);
+
+module.exports = router;
