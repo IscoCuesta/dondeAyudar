cef77dcf8c29ed090b528e55d131a7a022164a84
Magpie <edchavez821@gmail.com>
2019-08-07 18:27:58 -0500

    mongo models and api routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/dondeayudar");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dondeayudar");
 
 // Start the API server
 app.listen(PORT, function() {
