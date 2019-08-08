2785327e4d595a511f7dd343a1c97f7f7ef2ab76
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-08-07 19:24:23 -0500

    Merging dummy and Edgar1

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  nombre: { type: String, required: true },
+  tipo: { type: String, required: true },
+  necesidad: { type: String, required: true },
+  resumen: { type: String, required: true },
+  descripcion: { type: String, required: true },
+  imagen: { type: String, required: true },
+  fechaInicial: { type: Date, required: true },
+  fechaFinal: { type: Date, required: true },
+  lugar: { type: String, required: true },
+  link: { type: String, required: true },
+  organization: {
+    type: Schema.Types.ObjectId,
+    ref: "Organization"
+  }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
