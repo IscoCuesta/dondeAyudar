3a12623a6e512495cb9897b32a6c707d2384e60b
Magpie <edchavez821@gmail.com>
2019-08-17 10:35:41 -0500

    heroku 2

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const keys = {
+  apiKey: "AIzaSyAhsiKzucDSOqebtNPJUb21Q3BS6XRiO3s",
+  authDomain: "dondeayudar.firebaseapp.com",
+  databaseURL: "https://dondeayudar.firebaseio.com",
+  projectId: "dondeayudar",
+  storageBucket: "dondeayudar.appspot.com",
+  messagingSenderId: "874528403710",
+  appId: "1:874528403710:web:3f8f01ef649e1dbf"
+};
+
+export default keys;
