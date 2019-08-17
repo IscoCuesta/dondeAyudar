61d41557dd610125fcdc227cc65a1d917aa9e8a1
Magpie <edchavez821@gmail.com>
2019-08-17 11:05:45 -0500

    heroku deploy

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import firebase from "firebase";
-import keys from "./firebaseConfig.js"
+import keys from "./firebase-config.js"
 
 firebase.initializeApp(keys);
 
