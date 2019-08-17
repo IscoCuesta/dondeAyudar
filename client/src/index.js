155062efe111ebde1838f7039af33c23821534f3
Isco <Iscocuesta@hotmail.com>
2019-08-16 22:07:12 -0500

    firebaseconfig commented

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import firebase from "firebase";
-import keys from "./firebaseConfig";
+// import keys from "./firebaseConfig";
 require('dotenv').config()
 
 firebase.initializeApp(
